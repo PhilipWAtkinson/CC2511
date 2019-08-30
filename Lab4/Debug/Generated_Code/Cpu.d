50d78da827a330e60dc2ded9b2d4c79de95bd8d0
Philip <philip.atkinson@my.jcu.edu.au>
2019-08-30 12:00:14 +1000

    Lab 4
    Lab 4 added

---
+++ Lab4/Debug/Generated_Code/Cpu.d
@@ -0,0 +1,82 @@
+Generated_Code/Cpu.o: ../Generated_Code/Cpu.c ../Generated_Code/Blue.h \
+ ../Generated_Code/PE_Types.h ../Generated_Code/PE_Error.h \
+ ../Generated_Code/PE_Const.h ../Generated_Code/IO_Map.h \
+ C:/Users/Philip/workspace.kds/Lab4/Static_Code/IO_Map/MK20D5.h \
+ ../Generated_Code/BitIoLdd1.h \
+ C:/Users/Philip/workspace.kds/Lab4/Static_Code/PDD/GPIO_PDD.h \
+ C:/Users/Philip/workspace.kds/Lab4/Static_Code/PDD/PDD_Types.h \
+ ../Generated_Code/Cpu.h ../Generated_Code/Red.h \
+ ../Generated_Code/BitIoLdd2.h ../Generated_Code/Green.h \
+ ../Generated_Code/BitIoLdd3.h ../Generated_Code/AS1.h \
+ ../Generated_Code/ASerialLdd1.h \
+ C:/Users/Philip/workspace.kds/Lab4/Sources/Events.h \
+ C:/Users/Philip/workspace.kds/Lab4/Generated_Code/PE_Types.h \
+ C:/Users/Philip/workspace.kds/Lab4/Generated_Code/PE_Error.h \
+ C:/Users/Philip/workspace.kds/Lab4/Generated_Code/PE_Const.h \
+ C:/Users/Philip/workspace.kds/Lab4/Generated_Code/IO_Map.h \
+ C:/Users/Philip/workspace.kds/Lab4/Generated_Code/Blue.h \
+ C:/Users/Philip/workspace.kds/Lab4/Generated_Code/BitIoLdd1.h \
+ C:/Users/Philip/workspace.kds/Lab4/Generated_Code/Red.h \
+ C:/Users/Philip/workspace.kds/Lab4/Generated_Code/BitIoLdd2.h \
+ C:/Users/Philip/workspace.kds/Lab4/Generated_Code/Green.h \
+ C:/Users/Philip/workspace.kds/Lab4/Generated_Code/BitIoLdd3.h \
+ C:/Users/Philip/workspace.kds/Lab4/Generated_Code/AS1.h \
+ C:/Users/Philip/workspace.kds/Lab4/Generated_Code/ASerialLdd1.h
+
+../Generated_Code/Blue.h:
+
+../Generated_Code/PE_Types.h:
+
+../Generated_Code/PE_Error.h:
+
+../Generated_Code/PE_Const.h:
+
+../Generated_Code/IO_Map.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Static_Code/IO_Map/MK20D5.h:
+
+../Generated_Code/BitIoLdd1.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Static_Code/PDD/GPIO_PDD.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Static_Code/PDD/PDD_Types.h:
+
+../Generated_Code/Cpu.h:
+
+../Generated_Code/Red.h:
+
+../Generated_Code/BitIoLdd2.h:
+
+../Generated_Code/Green.h:
+
+../Generated_Code/BitIoLdd3.h:
+
+../Generated_Code/AS1.h:
+
+../Generated_Code/ASerialLdd1.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Sources/Events.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Generated_Code/PE_Types.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Generated_Code/PE_Error.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Generated_Code/PE_Const.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Generated_Code/IO_Map.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Generated_Code/Blue.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Generated_Code/BitIoLdd1.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Generated_Code/Red.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Generated_Code/BitIoLdd2.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Generated_Code/Green.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Generated_Code/BitIoLdd3.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Generated_Code/AS1.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Generated_Code/ASerialLdd1.h:
---
+++ Lab4/Debug/Generated_Code/Red.d
@@ -0,0 +1,28 @@
+Generated_Code/Red.o: ../Generated_Code/Red.c ../Generated_Code/Red.h \
+ ../Generated_Code/PE_Types.h ../Generated_Code/PE_Error.h \
+ ../Generated_Code/PE_Const.h ../Generated_Code/IO_Map.h \
+ C:/Users/Philip/workspace.kds/Lab4/Static_Code/IO_Map/MK20D5.h \
+ ../Generated_Code/BitIoLdd2.h \
+ C:/Users/Philip/workspace.kds/Lab4/Static_Code/PDD/GPIO_PDD.h \
+ C:/Users/Philip/workspace.kds/Lab4/Static_Code/PDD/PDD_Types.h \
+ ../Generated_Code/Cpu.h
+
+../Generated_Code/Red.h:
+
+../Generated_Code/PE_Types.h:
+
+../Generated_Code/PE_Error.h:
+
+../Generated_Code/PE_Const.h:
+
+../Generated_Code/IO_Map.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Static_Code/IO_Map/MK20D5.h:
+
+../Generated_Code/BitIoLdd2.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Static_Code/PDD/GPIO_PDD.h:
+
+C:/Users/Philip/workspace.kds/Lab4/Static_Code/PDD/PDD_Types.h:
+
+../Generated_Code/Cpu.h:
